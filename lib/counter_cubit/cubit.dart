ab46ec7c3e98527d4866793fb74c681db2625161
Aliaksandr Tserashkou <ter.alexanderandr@gmail.com>
2023-11-05 13:20:30 +0100

    Add cubit version with a state

--- lib/counter_cubit/cubit.dart
+++ lib/counter_cubit/cubit.dart
@@ -9,3 +9,20 @@ class CounterCubit extends Cubit<int> {
   /// Subtract 1 from the current state.
   void decrement() => emit(state - 1);
 }
+
+/*
+class CounterCubitTest extends Cubit<CounterCubitStateTest> {
+  CounterCubitTest() : super(const CounterCubitStateTest(0));
+
+  /// Add 1 to the current state.
+  void increment() => emit(CounterCubitStateTest(state.counter + 1));
+
+  /// Subtract 1 from the current state.
+  void decrement() => emit(CounterCubitStateTest(state.counter + 1));
+}
+
+class CounterCubitStateTest {
+  final int counter;
+  const CounterCubitStateTest(this.counter);
+}
+*/
